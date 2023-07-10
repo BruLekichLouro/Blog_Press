151b38b85507a10b8653f7bfe1cc8e84a256c6e9
BruLekichLouro <bububio@yahoo.com.br>
2023-07-10 16:03:38 -0300

    Criando rota para formulário de criação de usuário

--- users/UserController.js
+++ users/UserController.js
@@ -5,8 +5,15 @@ const User = require("./User")
 router.get("/admin/users", (req, res) =>{
     res.send("Listagem de usuários")
 });
+
 router.get("/admin/users/create", (req, res)=>{
     res.render("admin/users/create")
+});
+
+router.post("/users/create", (req, res)=>{
+    var email= req.body.email;
+    var password = req.body.password;
+    res.json({email, password});
 })
 
 module.exports= router;
