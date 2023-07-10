b0513392dd16e15b3ef95e744ed3a5ecb5f07a70
BruLekichLouro <bububio@yahoo.com.br>
2023-07-10 18:32:27 -0300

    Adicionando biblioteca para gerar hash, gerar hash da senha e enviar para o bd

--- users/UserController.js
+++ users/UserController.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const router = express.Router();
-const User = require("./User")
+const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) =>{
     res.send("Listagem de usuários")
@@ -13,7 +14,21 @@ router.get("/admin/users/create", (req, res)=>{
 router.post("/users/create", (req, res)=>{
     var email= req.body.email;
     var password = req.body.password;
-    res.json({email, password});
-})
+
+    //Criando hash e enviando para o bd:
+        //para aumentar a segurança:
+        var salt = bcrypt.genSaltSync(10);
+        //gerando o hash:
+        var hash = bcrypt.hashSync(password, salt);
+
+        User.create({
+            email:email,
+            password:hash
+        }).then(()=>{
+            res.redirect("/");
+        }).catch((err)=>{
+            res.redirect("/");
+        });
+});
 
 module.exports= router;
