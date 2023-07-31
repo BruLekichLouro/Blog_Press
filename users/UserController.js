cbb4e8b5c18472278adee520a682f3710e15af33
BruLekichLouro <bububio@yahoo.com.br>
2023-07-31 18:37:02 -0300

    Adicionando autorização com middleware

--- users/UserController.js
+++ users/UserController.js
@@ -41,33 +41,34 @@ router.post("/users/create", (req, res)=>{
 });
 
 //Rota pág de login
-router.get("/login", (req, res) => {
+router.get("/admin/login", (req, res) => {
     res.render("admin/users/login")
 });
 
-//Rota para login
+//Rota para autenticação
 router.post("/authenticate", (req, res)=>{
     var email = req.body.email;
     var password = req.body.password;
 
     User.findOne({where: {email:email}}).then(user =>{
         if(user != undefined){
+            console.log(user.password)
             //Se exite usuário com este email, vou validar a senha comparando a password digitada hasheada(na pág de login)  com a hasheada salva no BD
             var correct = bcrypt.compareSync(password, user.password);
 
             if(correct){ //criar sessão chamada user que só tem acesso quem faz login:
-                req.session.user={
-                    id:user.id,
-                    email:user.email
+                req.session.user = {
+                    id: user.id,
+                    email: user.email
                 }
-                res.json(req.session.user)
+                res.redirect("/admin/articles")
             }else{
-                res.redirect("/login");
+                res.redirect("/admin/login");
             }
         }else{
-            res.redirect("/login");
+            res.redirect("/admin/login");
         }
-    })
-})
+    }); 
+});
 
 module.exports= router;
